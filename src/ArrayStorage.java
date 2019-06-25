ec7021d7fbb66d54303f223da8fe992f617448a2
Alex Semkin <klaussr@mail.ru>
2019-06-25 14:23:56 +1000

    Forth try

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,41 +6,45 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int size = size();
+    int size = 0;
 
     void clear() {
-        for (int i = 0; i < size; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
-        }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                return;
-            }
+        int i = size();
+        while (i < storage.length) {
+            storage[i] = r;
+            size++;
+            return;
         }
+
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++){
-            if(storage[i].toString().equals(uuid)) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
                 return storage[i];
-            }
-            else return null;
+            } else return null;
         }
         return storage[uuid.indexOf(uuid)];
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++){
-            if (storage[i].toString().equals(uuid)){
-                storage[i] = null;
+        Resume[] result = new Resume[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equalsIgnoreCase(uuid)) {
+                index = i;
             }
         }
+        System.arraycopy(storage, 0, result, 0, index);
+        System.arraycopy(storage, index + 1, result, index, size - index);
+        for (int i = 0; i < result.length; i++) {
+            storage[i] = result[i];
+        }
+        size--;
     }
 
     /**
@@ -49,23 +53,13 @@ public class ArrayStorage {
     Resume[] getAll() {
         size();
         Resume[] storageNotNull = new Resume[size];
-        int j = 0;
-        for (int i = 0; i < size+1; i++) {
-            if (storage[i] != null) {
-                storageNotNull[j] = storage[i];
-                j++;
-            }
+        for (int i = 0; i < size; i++) {
+            storageNotNull[i] = storage[i];
         }
         return storageNotNull;
     }
 
     int size() {
-        size = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
